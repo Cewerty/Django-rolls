54d8fe0253bd163427f50bff043251ca0cd24f26
Cewerty <88933231+Cewerty@users.noreply.github.com>
2025-10-20 20:02:51 +0500

    Finished MVP for Django client for FastAPI RestAPI

--- dice/views.py
+++ dice/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from .models import Weapon
+from .models import Weapon, Armor, ArmorType, WeaponType
+from django.db.models import Prefetch
 
 def weapon_list(request):
-    weapons = Weapon.objects.all().order_by('-name')
-    return render(request, "dice/catalog.html", {'weapons': weapons})
+    weapon_types = WeaponType.objects.order_by("name").prefetch_related(
+        Prefetch("weapons", queryset=Weapon.objects.select_related("weapon_type").order_by("weapon_type__name", "name"))
+    )
+    armor_types = ArmorType.objects.order_by("name").prefetch_related(
+        Prefetch("armors", queryset=Armor.objects.select_related("armor_type").order_by("armor_type__name", "name"))
+    )
+    return render(request, "dice/catalog.html", {'weapon_types': weapon_types,
+                                                 'armor_types': armor_types,
+                                                 'dice_api_url': "/api/roll",
+                                                 })
