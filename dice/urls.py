26822b3b11aa18f5ce6614457b9961b8f0176319
Cewerty <88933231+Cewerty@users.noreply.github.com>
2025-10-20 17:54:48 +0500

    Edited html pages with views and models with them

--- dice/urls.py
+++ dice/urls.py
@@ -5,5 +5,4 @@ app_name = 'dice'
 
 urlpatterns = [
     path('', views.weapon_list, name='weapon_list'),
-    path('dice/<int:pk>/', views.weapon_detail, name='weapon_detail'),
     ]
--- dice/views.py
+++ dice/views.py
@@ -1,10 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import Weapon
 
 def weapon_list(request):
     weapons = Weapon.objects.all().order_by('-name')
-    return render(request, "dice/post_list.html", {'weapons': weapons})
-
-def weapon_detail(request, pk):
-    weapon = get_object_or_404(Weapon, pk=pk)
-    return render(request, 'dice/post_detail.html', {'weapon': weapon})
+    return render(request, "dice/catalog.html", {'weapons': weapons})
